b56ec2ef4249c66e1a894d19cf101077c8b68261
Karsten Reincke <k.reincke@telekom.de>
2013-03-12 13:56:58 +0100

    2013-03-12: psikeds-gtd-data initialized

---
+++ gtd-board/templates/psikeds-gtd.sh
@@ -0,0 +1,5 @@
+#/bin/sh
+#LOCALREPS=/home/reincke/gits.remote.world
+LOCALREPS=""
+PSIKEDSREP=${LOCALREPS}/psikeds/
+java -jar "/usr/local/lib/java/gtd-free-0.6-beta.jar" --data ${PSIKEDSREP}/gtd-board/
