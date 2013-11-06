2e6f23a758928f64e9d92fc0f4152321955ccecb
Marco Juliano <marco@juliano.de>
2013-11-06 15:46:30 +0100

    changed logging and tracing

--- ResolutionEngine/src/main/java/org/psikeds/resolutionengine/resolver/impl/DecissionEvaluator.java
+++ ResolutionEngine/src/main/java/org/psikeds/resolutionengine/resolver/impl/DecissionEvaluator.java
@@ -35,7 +35,7 @@ import org.psikeds.resolutionengine.resolver.Resolver;
  * Choice to just one Variant so that the AutoCompletion-Resolver afterwards
  * constructs new Knowledge-Entities for it.
  * If no Decission is supplied, nothing happens. Metadata will be ignored.
- *
+ * 
  * @author marco@juliano.de
  */
 public class DecissionEvaluator implements Resolver {
@@ -43,57 +43,100 @@ public class DecissionEvaluator implements Resolver {
   private static final Logger LOGGER = LoggerFactory.getLogger(DecissionEvaluator.class);
 
   /**
-   * @param knowledge            current old Knowledge
-   * @param decission            Decission (can be null)
-   * @param metadata             Metadata (ignored)
-   * @return Knowledge           resulting new Knowledge
-   * @throws ResolutionException if any error occurs
+   * @param knowledge
+   *          current old Knowledge
+   * @param decission
+   *          Decission (can be null)
+   * @param metadata
+   *          Metadata (ignored)
+   * @return Knowledge resulting new Knowledge
+   * @throws ResolutionException
+   *           if any error occurs
    */
   @Override
   public Knowledge resolve(final Knowledge knowledge, final Decission decission, final Metadata metadata) throws ResolutionException {
+    boolean ok = false;
+    boolean found = false;
     try {
-      LOGGER.trace("--> resolve(); Knowledge = {} \n Decission = {}", knowledge, decission);
-      if (knowledge == null) {
-        throw new ResolutionException("Cannot evaluate Decission: Knowledge is null!");
-      }
+      LOGGER.debug("Evaluating made Decission ...");
       if (decission != null) {
         if (StringUtils.isEmpty(decission.getPurposeID()) || StringUtils.isEmpty(decission.getVariantID())) {
-          throw new ResolutionException("Cannot evaluate, illegal Decission: " + decission);
+          final String errmsg = String.format("Cannot evaluate, illegal Decission: %s", decission);
+          LOGGER.warn(errmsg);
+          throw new ResolutionException(errmsg);
         }
-        final List<Choice> choices = knowledge.getChoices();
-        boolean found = updateChoices(choices, decission);
-        for (final KnowledgeEntity ke : knowledge.getEntities()) {
-          found = found | updateChoices(ke, decission);
-        }
+        found = updateKnowledge(knowledge, decission, metadata);
       }
       else {
         LOGGER.debug("Skipping Evaluation, Decission is null.");
       }
+      ok = true;
       return knowledge;
     }
     finally {
-      LOGGER.trace("<-- resolve(); new Knowledge = {}", knowledge);
+      LOGGER.debug("... finished evaluating made Decission. " + (found ? "" : "NOT") + " found, " + (ok ? "OK." : "ERROR!"));
     }
   }
 
-  private boolean updateChoices(final KnowledgeEntity ke, final Decission decission) throws ResolutionException {
-    boolean found = updateChoices(ke.getChoices(), decission);
-    for (final KnowledgeEntity sibling : ke.getSiblings()) {
-      found = found | updateChoices(sibling, decission);
-    }
-    return found;
-  }
-
-  private boolean updateChoices(final List<Choice> choices, final Decission decission) throws ResolutionException {
+  private boolean updateKnowledge(final Knowledge knowledge, final Decission decission, final Metadata metadata) throws ResolutionException {
     boolean found = false;
-    for (final Choice c : choices) {
-      final Variant v = c.matches(decission);
-      if (v != null) {
-        LOGGER.debug("Found Choice matching Decission.\nd = {}\nc = {}", decission, c);
-        c.setVariant(v);
-        found = true;
+    try {
+      LOGGER.trace("--> updateKnowledge: Knowledge = {}\nDecission = {}", knowledge, decission);
+      if (knowledge == null) {
+        final String errmsg = "Cannot evaluate Decission: Knowledge is null!";
+        LOGGER.warn(errmsg);
+        throw new ResolutionException(errmsg);
       }
+      final List<Choice> choices = knowledge.getChoices();
+      found = updateChoices(choices, decission, metadata);
+      for (final KnowledgeEntity ke : knowledge.getEntities()) {
+        found = found | updateEntity(ke, decission, metadata);
+      }
+      return found;
     }
-    return found;
+    finally {
+      LOGGER.trace("<-- updateKnowledge: Found = {}\nKnowledge = {}\nDecission = {}", found, knowledge, decission);
+    }
+  }
+
+  private boolean updateEntity(final KnowledgeEntity ke, final Decission decission, final Metadata metadata) throws ResolutionException {
+    boolean found = false;
+    try {
+      LOGGER.trace("--> updateEntity: KnowledgeEntity = {}", ke);
+      found = updateChoices(ke.getChoices(), decission, metadata);
+      for (final KnowledgeEntity sibling : ke.getSiblings()) {
+        found = found | updateEntity(sibling, decission, metadata);
+      }
+      return found;
+    }
+    finally {
+      LOGGER.trace("<-- updateEntity: Found = {}\nKnowledgeEntity = {}", found, ke);
+    }
+  }
+
+  private boolean updateChoices(final List<Choice> choices, final Decission decission, final Metadata metadata) throws ResolutionException {
+    boolean found = false;
+    try {
+      LOGGER.trace("--> updateChoices: Choices = {}", choices);
+      for (final Choice c : choices) {
+        final Variant v = c.matches(decission);
+        if (v != null) {
+          c.setVariant(v);
+          found = true;
+          decissionMessage(metadata, decission, c);
+        }
+      }
+      return found;
+    }
+    finally {
+      LOGGER.trace("<-- updateChoices: Found = {}\nChoices = {}", found, choices);
+    }
+  }
+
+  private void decissionMessage(final Metadata metadata, final Decission decission, final Choice c) {
+    final String key = String.format("Decission%d", System.currentTimeMillis());
+    final String msg = String.format("Found Choice matching Decission.\nDecission = %s\nChoice = %s", decission, c);
+    LOGGER.debug(msg);
+    metadata.saveInfo(key, msg);
   }
 }
