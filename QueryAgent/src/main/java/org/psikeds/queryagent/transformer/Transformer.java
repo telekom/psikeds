5af7d5b9ad8fc5284815d9409aa9a48f022a85fb
Marco Juliano <marco@juliano.de>
2013-11-12 15:14:18 +0100

    chg: update for new interface version

--- QueryAgent/src/main/java/org/psikeds/queryagent/transformer/Transformer.java
+++ QueryAgent/src/main/java/org/psikeds/queryagent/transformer/Transformer.java
@@ -14,103 +14,68 @@
  *******************************************************************************/
 package org.psikeds.queryagent.transformer;
 
-import org.psikeds.queryagent.interfaces.presenter.pojos.Alternatives;
-import org.psikeds.queryagent.interfaces.presenter.pojos.Constituents;
-import org.psikeds.queryagent.interfaces.presenter.pojos.Constitutes;
-import org.psikeds.queryagent.interfaces.presenter.pojos.Event;
-import org.psikeds.queryagent.interfaces.presenter.pojos.Events;
+import org.psikeds.queryagent.interfaces.presenter.pojos.Choice;
+import org.psikeds.queryagent.interfaces.presenter.pojos.Decission;
 import org.psikeds.queryagent.interfaces.presenter.pojos.Feature;
-import org.psikeds.queryagent.interfaces.presenter.pojos.Features;
-import org.psikeds.queryagent.interfaces.presenter.pojos.Fulfills;
-import org.psikeds.queryagent.interfaces.presenter.pojos.InitResponse;
+import org.psikeds.queryagent.interfaces.presenter.pojos.FeatureValueType;
+import org.psikeds.queryagent.interfaces.presenter.pojos.Knowledge;
 import org.psikeds.queryagent.interfaces.presenter.pojos.KnowledgeEntity;
+import org.psikeds.queryagent.interfaces.presenter.pojos.Metadata;
 import org.psikeds.queryagent.interfaces.presenter.pojos.Purpose;
-import org.psikeds.queryagent.interfaces.presenter.pojos.Purposes;
-import org.psikeds.queryagent.interfaces.presenter.pojos.Rule;
-import org.psikeds.queryagent.interfaces.presenter.pojos.Rules;
-import org.psikeds.queryagent.interfaces.presenter.pojos.SelectRequest;
-import org.psikeds.queryagent.interfaces.presenter.pojos.SelectResponse;
+import org.psikeds.queryagent.interfaces.presenter.pojos.ResolutionRequest;
+import org.psikeds.queryagent.interfaces.presenter.pojos.ResolutionResponse;
 import org.psikeds.queryagent.interfaces.presenter.pojos.Variant;
-import org.psikeds.queryagent.interfaces.presenter.pojos.Variants;
 
 /**
  * Helper for transforming POJOs from the Interface of the
  * Resolution-Engine (RE) into POJOs of the Interface of the
  * Query-Agent (QA) ... and vice versa.
- *
+ * 
  * @author marco@juliano.de
  */
 public interface Transformer {
 
-  Alternatives re2qa(final org.psikeds.resolutionengine.interfaces.pojos.Alternatives re);
+  Choice re2qa(final org.psikeds.resolutionengine.interfaces.pojos.Choice re);
 
-  org.psikeds.resolutionengine.interfaces.pojos.Alternatives qa2re(final Alternatives qa);
+  org.psikeds.resolutionengine.interfaces.pojos.Choice qa2re(final Choice qa);
 
-  Constituents re2qa(final org.psikeds.resolutionengine.interfaces.pojos.Constituents re);
+  Decission re2qa(final org.psikeds.resolutionengine.interfaces.pojos.Decission re);
 
-  org.psikeds.resolutionengine.interfaces.pojos.Constituents qa2re(final Constituents qa);
-
-  Constitutes re2qa(final org.psikeds.resolutionengine.interfaces.pojos.Constitutes re);
-
-  org.psikeds.resolutionengine.interfaces.pojos.Constitutes qa2re(final Constitutes qa);
-
-  Event re2qa(final org.psikeds.resolutionengine.interfaces.pojos.Event re);
-
-  org.psikeds.resolutionengine.interfaces.pojos.Event qa2re(final Event qa);
-
-  Events re2qa(final org.psikeds.resolutionengine.interfaces.pojos.Events re);
-
-  org.psikeds.resolutionengine.interfaces.pojos.Events qa2re(final Events qa);
+  org.psikeds.resolutionengine.interfaces.pojos.Decission qa2re(final Decission qa);
 
   Feature re2qa(final org.psikeds.resolutionengine.interfaces.pojos.Feature re);
 
   org.psikeds.resolutionengine.interfaces.pojos.Feature qa2re(final Feature qa);
 
-  Features re2qa(final org.psikeds.resolutionengine.interfaces.pojos.Features re);
+  FeatureValueType re2qa(final org.psikeds.resolutionengine.interfaces.pojos.FeatureValueType re);
 
-  org.psikeds.resolutionengine.interfaces.pojos.Features qa2re(final Features qa);
+  org.psikeds.resolutionengine.interfaces.pojos.FeatureValueType qa2re(final FeatureValueType qa);
 
-  Fulfills re2qa(final org.psikeds.resolutionengine.interfaces.pojos.Fulfills re);
+  Knowledge re2qa(final org.psikeds.resolutionengine.interfaces.pojos.Knowledge re);
 
-  org.psikeds.resolutionengine.interfaces.pojos.Fulfills qa2re(final Fulfills qa);
-
-  Purpose re2qa(final org.psikeds.resolutionengine.interfaces.pojos.Purpose re);
-
-  org.psikeds.resolutionengine.interfaces.pojos.Purpose qa2re(final Purpose qa);
-
-  Purposes re2qa(final org.psikeds.resolutionengine.interfaces.pojos.Purposes re);
-
-  org.psikeds.resolutionengine.interfaces.pojos.Purposes qa2re(final Purposes qa);
-
-  Rule re2qa(final org.psikeds.resolutionengine.interfaces.pojos.Rule re);
-
-  org.psikeds.resolutionengine.interfaces.pojos.Rule qa2re(final Rule qa);
-
-  Rules re2qa(final org.psikeds.resolutionengine.interfaces.pojos.Rules re);
-
-  org.psikeds.resolutionengine.interfaces.pojos.Rules qa2re(final Rules qa);
-
-  Variant re2qa(final org.psikeds.resolutionengine.interfaces.pojos.Variant re);
-
-  org.psikeds.resolutionengine.interfaces.pojos.Variant qa2re(final Variant qa);
-
-  Variants re2qa(final org.psikeds.resolutionengine.interfaces.pojos.Variants re);
-
-  org.psikeds.resolutionengine.interfaces.pojos.Variants qa2re(final Variants qa);
+  org.psikeds.resolutionengine.interfaces.pojos.Knowledge qa2re(final Knowledge qa);
 
   KnowledgeEntity re2qa(final org.psikeds.resolutionengine.interfaces.pojos.KnowledgeEntity re);
 
   org.psikeds.resolutionengine.interfaces.pojos.KnowledgeEntity qa2re(final KnowledgeEntity qa);
 
-  InitResponse re2qa(final org.psikeds.resolutionengine.interfaces.pojos.InitResponse re);
+  Metadata re2qa(final org.psikeds.resolutionengine.interfaces.pojos.Metadata re);
 
-  org.psikeds.resolutionengine.interfaces.pojos.InitResponse qa2re(final InitResponse qa);
+  org.psikeds.resolutionengine.interfaces.pojos.Metadata qa2re(final Metadata qa);
 
-  SelectRequest re2qa(final org.psikeds.resolutionengine.interfaces.pojos.SelectRequest re);
+  Purpose re2qa(final org.psikeds.resolutionengine.interfaces.pojos.Purpose re);
 
-  org.psikeds.resolutionengine.interfaces.pojos.SelectRequest qa2re(final SelectRequest qa);
+  org.psikeds.resolutionengine.interfaces.pojos.Purpose qa2re(final Purpose qa);
 
-  SelectResponse re2qa(final org.psikeds.resolutionengine.interfaces.pojos.SelectResponse re);
+  ResolutionRequest re2qa(final org.psikeds.resolutionengine.interfaces.pojos.ResolutionRequest re);
 
-  org.psikeds.resolutionengine.interfaces.pojos.SelectResponse qa2re(final SelectResponse qa);
+  org.psikeds.resolutionengine.interfaces.pojos.ResolutionRequest qa2re(final ResolutionRequest qa);
+
+  ResolutionResponse re2qa(final org.psikeds.resolutionengine.interfaces.pojos.ResolutionResponse re);
+
+  org.psikeds.resolutionengine.interfaces.pojos.ResolutionResponse qa2re(final ResolutionResponse qa);
+
+  Variant re2qa(final org.psikeds.resolutionengine.interfaces.pojos.Variant re);
+
+  org.psikeds.resolutionengine.interfaces.pojos.Variant qa2re(final Variant qa);
 }
