ae2c2da8b27691f22e02c6a4c75100b94b1eaef9
Marco Juliano <marco@juliano.de>
2013-12-07 09:35:45 +0100

    initial checkin

---
+++ build.bat
@@ -0,0 +1,27 @@
+@echo off
+@rem ---------------------------------------------------------------------------
+@rem psiKeds :- ps induced knowledge entity delivery system
+@rem
+@rem Copyright (c) 2013, 2014 Karsten Reincke, Marco Juliano, Deutsche Telekom AG
+@rem
+@rem This file is free software: you can redistribute
+@rem it and/or modify it under the terms of the
+@rem [x] GNU Affero General Public License
+@rem [ ] GNU General Public License
+@rem [ ] GNU Lesser General Public License
+@rem [ ] Creatice Commons ShareAlike License
+@rem
+@rem For details see file LICENSING in the top project directory 
+@rem ---------------------------------------------------------------------------
+@rem
+@rem This script performs a full built of psiKeds
+@rem
+@rem ---------------------------------------------------------------------------
+if "%OS%" == "Windows_NT" setlocal
+
+set "SCRIPT_DIR=%~dp0"
+set "CURRENT_DIR=%cd%"
+
+cd "%SCRIPT_DIR%"
+mvn clean install#
+cd "%CURRENT_DIR%"
