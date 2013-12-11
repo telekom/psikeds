b9ee24a0473a0f1fba807bd770f73b620e59b709
Marco Juliano <marco@juliano.de>
2013-12-11 15:32:56 +0100

    code cleanup; improved logging

--- Common/src/main/java/org/psikeds/common/threadlocal/ThreadLocalHelper.java
+++ Common/src/main/java/org/psikeds/common/threadlocal/ThreadLocalHelper.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * psiKeds :- ps induced knowledge entity delivery system
  *
- * Copyright (c) 2013 Karsten Reincke, Marco Juliano, Deutsche Telekom AG
+ * Copyright (c) 2013, 2014 Karsten Reincke, Marco Juliano, Deutsche Telekom AG
  *
  * This file is free software: you can redistribute
  * it and/or modify it under the terms of the
@@ -33,15 +33,15 @@ import org.apache.commons.lang.StringUtils;
  * Memory Leaks or Data leaking between Threads (e.g. when the Application
  * is undeployed from an Application Server and its Threads are reused for
  * handling requests of other Applications).
- *
+ * 
  * Problem is that (for very good reason!) you cannot access the local
  * variables of other Threads. So in order to clean the mess up, we will
  * use Reflection to circumvent the visibilty of Fields and Methods
  * (hopefully the Security-Manager does not prevent us!) and will then
  * try to perform some serious magic. ;-)
- *
+ * 
  * @author marco@juliano.de
- *
+ * 
  */
 public final class ThreadLocalHelper {
 
@@ -58,16 +58,16 @@ public final class ThreadLocalHelper {
   /**
    * Check all ThreadLocalMaps of all Threads for Objects created by our
    * Application (i.e. our ClassLoader) and remove those Entries.
-   *
+   * 
    * Note: Be careful, this Method is intended to be invoked by an
    * Application having a separate ClassLoader (e.g. WAR or EAR deployed
    * on an Application Server). Running it from within a standalone
    * Java-Program or IDE might have unexpected results!
-   *
+   * 
    */
   public static void cleanThreadLocalMaps() {
     try {
-      LOGGER.trace("--> cleanThreadLocalMaps(); enabled = " + enabled);
+      LOGGER.trace("--> cleanThreadLocalMaps(); enabled = {}", enabled);
 
       // For each Thread its ThreadLocals are stored in the two Fields named
       // 'threadLocals' and 'inheritableThreadLocal' of the inner Type/Class
@@ -98,33 +98,30 @@ public final class ThreadLocalHelper {
       final Thread[] threads = getThreads();
       final int numThreads = threads == null ? 0 : threads.length;
       for (int i = 0; i < numThreads; i++) {
-        if (threads[i] != null) {
-          final Thread t = threads[i];
-          if (t != null) {
-            LOGGER.debug("Checking ThreadLocalMap of " + t);
-
-            // Clear the first map (Thread.threadLocals)
-            Object threadLocalMap = threadLocalsField.get(t);
-            if (null != threadLocalMap) {
-              expungeStaleEntriesMethod.invoke(threadLocalMap);
-              cleanThreadLocalMaps(threadLocalMap, tableField, removeThreadLocalMethod);
-            }
-
-            // Clear the second map (Thread.inheritableThreadLocals)
-            threadLocalMap = inheritableThreadLocalsField.get(t);
-            if (null != threadLocalMap) {
-              expungeStaleEntriesMethod.invoke(threadLocalMap);
-              cleanThreadLocalMaps(threadLocalMap, tableField, removeThreadLocalMethod);
-            }
+        final Thread t = threads[i];
+        if (t != null) {
+          LOGGER.debug("Checking ThreadLocalMap of {}", t);
+          // Clear the first map (Thread.threadLocals)
+          Object threadLocalMap = threadLocalsField.get(t);
+          if (threadLocalMap != null) {
+            expungeStaleEntriesMethod.invoke(threadLocalMap);
+            cleanThreadLocalMaps(threadLocalMap, tableField, removeThreadLocalMethod);
+          }
+          // Clear the second map (Thread.inheritableThreadLocals)
+          threadLocalMap = inheritableThreadLocalsField.get(t);
+          if (threadLocalMap != null) {
+            expungeStaleEntriesMethod.invoke(threadLocalMap);
+            cleanThreadLocalMaps(threadLocalMap, tableField, removeThreadLocalMethod);
           }
         }
       }
     }
     catch (final Throwable t) {
+      // Just catch anything, we do not want to let the Application fail because of this.
       LOGGER.warn("Could not clean ThreadLocalMaps: " + t.getMessage(), t);
     }
     finally {
-      LOGGER.trace("<-- cleanThreadLocalMaps(); enabled = " + enabled);
+      LOGGER.trace("<-- cleanThreadLocalMaps(); enabled = {}", enabled);
     }
   }
 
@@ -132,49 +129,47 @@ public final class ThreadLocalHelper {
 
   /**
    * Get all active Threads of our Root-ThreadGroup.
-   *
+   * 
    * @return Array of Threads
    */
   private static Thread[] getThreads() {
-
     int threadCountActual = 0;
-    int threadCountGuess = 0;
-    Thread[] threads = null;
-
     try {
       LOGGER.trace("--> getThreads()");
-
       ThreadGroup rootgroup = Thread.currentThread().getThreadGroup();
       while (rootgroup.getParent() != null) {
         rootgroup = rootgroup.getParent();
       }
-
       // Note: ThreadGroup.enumerate(Thread[]) silently ignores any Thread
       // that won't fit into the Array. Therefore we must make sure that
       // we do not miss any Threads by continuously increasing the Size of
       // the Array.
-
-      threadCountGuess = rootgroup.activeCount();
+      Thread[] threads = null;
+      int threadCountGuess = rootgroup.activeCount();
       do {
         threadCountGuess *= 2;
         threads = new Thread[threadCountGuess];
         threadCountActual = rootgroup.enumerate(threads);
-      } while (threadCountActual == threadCountGuess);
-
+      }
+      while (threadCountActual == threadCountGuess);
       return threads;
     }
     finally {
-      LOGGER.trace("<-- getThreads(); #Threads = " + threadCountActual);
+      LOGGER.trace("<-- getThreads(); #Threads = {}", threadCountActual);
     }
   }
 
   /**
    * Analyzes a given ThreadLocalMap and remove all Entries loaded by our ClassLoader/Application.
-   *
-   * @param map ThreadLocalMap to be analyzed and cleaned.
-   * @param internalTableField Actual Field pointing to 'table'; This avoids re-calculation on every call to this method.
-   * @param removeThreadLocalMethod Method to be invoked for removing an Entry
-   *
+   * 
+   * @param map
+   *          ThreadLocalMap to be analyzed and cleaned.
+   * @param internalTableField
+   *          Actual Field pointing to 'table'; This avoids re-calculation on every call to this
+   *          method.
+   * @param removeThreadLocalMethod
+   *          Method to be invoked for removing an Entry
+   * 
    * @throws IllegalArgumentException
    * @throws IllegalAccessException
    * @throws SecurityException
@@ -183,11 +178,9 @@ public final class ThreadLocalHelper {
    */
   private static void cleanThreadLocalMaps(final Object threadLocalMap, final Field internalTableField, final Method removeThreadLocalMethod)
       throws IllegalArgumentException, IllegalAccessException, SecurityException, NoSuchFieldException, InvocationTargetException {
-
     try {
       LOGGER.trace("--> cleanThreadLocalMaps()");
-
-      final Object[] table = threadLocalMap == null || internalTableField == null ? null : (Object[]) internalTableField.get(threadLocalMap);
+      final Object[] table = ((threadLocalMap == null) || (internalTableField == null)) ? null : (Object[]) internalTableField.get(threadLocalMap);
       final int tablength = table == null ? 0 : table.length;
       for (int i = 0; i < tablength; i++) {
         final Object obj = table[i];
@@ -195,14 +188,14 @@ public final class ThreadLocalHelper {
           boolean shouldBeRemoved = false;
           // Check the key
           final Object key = ((Reference<?>) obj).get();
-          if (loadedByOurCLorChild(key)) {
+          if (loadedByOurClassloaderOrChild(key)) {
             shouldBeRemoved = true;
           }
           // Check the value
           final Field valueField = obj.getClass().getDeclaredField("value");
           valueField.setAccessible(true);
           final Object value = valueField.get(obj);
-          if (loadedByOurCLorChild(value)) {
+          if (loadedByOurClassloaderOrChild(value)) {
             shouldBeRemoved = true;
           }
           if (shouldBeRemoved) {
@@ -218,28 +211,31 @@ public final class ThreadLocalHelper {
 
   /**
    * Remove a a given Object (key + value) from the ThreadLocalMap.
-   *
-   * @param internalTableField Field pointing to 'table' holding the ThreadLocals.
-   * @param key Key of the Object to be removed
-   * @param value Value of the Object to be removed
-   * @param removeThreadLocalMethod Method to be invoked for removing an Entry
-   *
+   * 
+   * @param internalTableField
+   *          Field pointing to 'table' holding the ThreadLocals.
+   * @param key
+   *          Key of the Object to be removed
+   * @param value
+   *          Value of the Object to be removed
+   * @param removeThreadLocalMethod
+   *          Method to be invoked for removing an Entry
+   * 
    * @throws IllegalArgumentException
    * @throws IllegalAccessException
    * @throws InvocationTargetException
    */
   private static void remove(final Object threadLocalMap, final Object key, final Object value, final Method removeThreadLocalMethod)
       throws IllegalArgumentException, IllegalAccessException, InvocationTargetException {
-
     try {
       LOGGER.trace("--> remove()");
       final String objStr = prettyPrintObject(key, value);
       if (enabled) {
-        LOGGER.debug("Removing Object from ThreadLocal-Table: " + objStr);
+        LOGGER.debug("Removing Object from ThreadLocal-Table: {}", objStr);
         removeThreadLocalMethod.invoke(threadLocalMap, key);
       }
       else {
-        LOGGER.debug("Found Object that should be removed from ThreadLocal-Table: " + objStr);
+        LOGGER.debug("Found Object that should be removed from ThreadLocal-Table: {}", objStr);
       }
     }
     finally {
@@ -250,14 +246,15 @@ public final class ThreadLocalHelper {
   /**
    * Check whether an Object was loaded by the same ClassLoader (or one of its Childs)
    * as this Class, i.e. whether Object was created by our Application.
-   *
-   * @param obj Object to be checked.
+   * 
+   * @param obj
+   *          Object to be checked.
    * @return True if same ClassLoader was used, false else.
    */
-  private static boolean loadedByOurCLorChild(final Object obj) {
+  private static boolean loadedByOurClassloaderOrChild(final Object obj) {
     boolean ret = false;
     try {
-      LOGGER.trace("--> loadedByOurCLorChild()");
+      LOGGER.trace("--> loadedByOurClassloaderOrChild()");
       // no object, nothing to do
       if (obj == null) {
         LOGGER.trace("Object is null.");
@@ -267,9 +264,9 @@ public final class ThreadLocalHelper {
       // object loaded by same classloader as ourself?
       final ClassLoader ownCL = getOwnClassLoader();
       ClassLoader objCL = getClassLoader(obj);
-      while (ownCL != null && objCL != null) {
+      while ((ownCL != null) && (objCL != null)) {
         if (ownCL == objCL) {
-          LOGGER.trace("Object was loaded by our ClassLoader: " + obj);
+          LOGGER.trace("Object was loaded by our ClassLoader: {}", obj);
           ret = true;
           return ret;
         }
@@ -281,37 +278,38 @@ public final class ThreadLocalHelper {
         try {
           while (iter.hasNext()) {
             final Object entry = iter.next();
-            if (loadedByOurCLorChild(entry)) {
-              LOGGER.trace("Object is Collection and contains Entry loaded by our ClassLoader: " + entry);
+            if (loadedByOurClassloaderOrChild(entry)) {
+              LOGGER.trace("Object is Collection and contains Entry loaded by our ClassLoader: {}", entry);
               ret = true;
               return ret;
             }
           }
         }
         catch (final ConcurrentModificationException cme) {
-          LOGGER.warn("loadedByOurCLorChild: Concurrent modification of " + obj, cme);
+          LOGGER.warn("loadedByOurClassloaderOrChild: Concurrent modification of " + String.valueOf(obj), cme);
         }
       }
-      LOGGER.trace("Object was not loaded by our ClassLoader/Applicatioon: " + obj);
+      LOGGER.trace("Object was not loaded by our ClassLoader/Application: {}", obj);
       ret = false;
       return ret;
     }
     finally {
-      LOGGER.trace("<-- loadedByOurCLorChild(); ret = " + ret);
+      LOGGER.trace("<-- loadedByOurClassloaderOrChild(); ret = {}", ret);
     }
   }
 
   /**
    * Get ClassLoader that loaded a given Object.
-   *
-   * @param obj Object
+   * 
+   * @param obj
+   *          Object
    * @return ClassLoader
    */
   private static ClassLoader getClassLoader(final Object obj) {
     Class<?> clazz = null;
     ClassLoader cl = null;
     try {
-      LOGGER.trace("--> getClassLoader()");
+      LOGGER.trace("--> getClassLoader(); obj = {}", obj);
       if (obj != null) {
         if (obj instanceof Class) {
           clazz = (Class<?>) obj;
@@ -320,22 +318,20 @@ public final class ThreadLocalHelper {
           clazz = obj.getClass();
         }
       }
-      if (clazz != null) {
-        cl = clazz.getClassLoader();
-      }
+      cl = (clazz == null ? null : clazz.getClassLoader());
       if (cl == null) {
         cl = ClassLoader.getSystemClassLoader();
       }
       return cl;
     }
     finally {
-      LOGGER.trace("<-- getClassLoader()\nclazz={}\n cl={}", clazz, cl);
+      LOGGER.trace("<-- getClassLoader(); clazz = {}; cl = {}", clazz, cl);
     }
   }
 
   /**
    * Get our own ClassLoader. Just for convenience.
-   *
+   * 
    * @return ClassLoader
    */
   private static ClassLoader getOwnClassLoader() {
@@ -344,11 +340,13 @@ public final class ThreadLocalHelper {
 
   /**
    * Create a String like: Key [ Class ] = Value [ Class ]
-   *
-   * @param key Key of the Object
-   * @param value Value of the Object
+   * 
+   * @param key
+   *          Key of the Object
+   * @param value
+   *          Value of the Object
    * @return String
-   *
+   * 
    */
   private static String prettyPrintObject(final Object key, final Object value) {
     final StringBuilder sb = new StringBuilder();
@@ -369,34 +367,41 @@ public final class ThreadLocalHelper {
 
   /**
    * Get canonical class name if possible, simple class name else.
-   *
-   * @param obj Object or Class
+   * 
+   * @param obj
+   *          Object or Class
    * @return String
-   *
+   * 
    */
   private static String getPrettyClassName(final Object obj) {
-    if (obj == null) {
-      return "";
-    }
-    else if (obj instanceof Class<?>) {
-      final Class<?> clazz = (Class<?>) obj;
-      String pcn = clazz.getCanonicalName();
-      if (StringUtils.isEmpty(pcn)) {
-        pcn = clazz.getName();
+    String pcn = null;
+    try {
+      if (obj != null) {
+        if (obj instanceof Class<?>) {
+          final Class<?> clazz = (Class<?>) obj;
+          pcn = clazz.getCanonicalName();
+          if (StringUtils.isEmpty(pcn)) {
+            pcn = clazz.getName();
+          }
+        }
+        else {
+          pcn = getPrettyClassName(obj.getClass());
+        }
       }
-      return pcn;
     }
-    else {
-      return getPrettyClassName(obj.getClass());
+    catch (final Throwable t) {
+      pcn = null;
     }
+    return (pcn == null ? "" : pcn);
   }
 
   /**
    * Paranoid version of tostring()
-   *
-   * @param obj Object
+   * 
+   * @param obj
+   *          Object
    * @return String
-   *
+   * 
    */
   private static String getPrettyObjectValue(final Object obj) {
     try {
