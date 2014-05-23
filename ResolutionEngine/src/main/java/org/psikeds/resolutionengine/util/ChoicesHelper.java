c2a36a38e46a34ce29a31e3a8290782e88640550
Marco Juliano <marco@juliano.de>
2014-05-23 09:16:40 +0200

    introduced new helpers

---
+++ ResolutionEngine/src/main/java/org/psikeds/resolutionengine/util/ChoicesHelper.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * psiKeds :- ps induced knowledge entity delivery system
+ *
+ * Copyright (c) 2013, 2014 Karsten Reincke, Marco Juliano, Deutsche Telekom AG
+ *
+ * This file is free software: you can redistribute
+ * it and/or modify it under the terms of the
+ * [x] GNU Affero General Public License
+ * [ ] GNU General Public License
+ * [ ] GNU Lesser General Public License
+ * [ ] Creatice Commons ShareAlike License
+ *
+ * For details see file LICENSING in the top project directory
+ *******************************************************************************/
+package org.psikeds.resolutionengine.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Helper for handling/manipulating all Kind of Choices
+ * 
+ * @author marco@juliano.de
+ * 
+ */
+public class ChoicesHelper {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(ChoicesHelper.class);
+
+  // TODO
+}
