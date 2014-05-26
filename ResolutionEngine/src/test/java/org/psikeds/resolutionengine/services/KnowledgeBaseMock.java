6ebefcc16b794c791a911c79ec6620678f83dbe4
Marco Juliano <marco@juliano.de>
2014-05-26 15:43:05 +0200

    Implemented getAttachedConcepts()

--- ResolutionEngine/src/test/java/org/psikeds/resolutionengine/services/KnowledgeBaseMock.java
+++ ResolutionEngine/src/test/java/org/psikeds/resolutionengine/services/KnowledgeBaseMock.java
@@ -422,6 +422,16 @@ public class KnowledgeBaseMock implements KnowledgeBase {
 
   /**
    * @param variantId
+   * @return Concepts
+   * @see org.psikeds.resolutionengine.datalayer.knowledgebase.KnowledgeBase#getAttachedConcepts(java.lang.String)
+   */
+  @Override
+  public Concepts getAttachedConcepts(final String variantId) {
+    return getConcepts();
+  }
+
+  /**
+   * @param variantId
    * @return Events
    * @see org.psikeds.resolutionengine.datalayer.knowledgebase.KnowledgeBase#getAttachedEvents(java.lang.String)
    */
