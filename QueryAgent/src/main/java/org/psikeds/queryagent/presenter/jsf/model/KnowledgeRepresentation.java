dbebfab7af9ace5dc78988cde14b33fd4e0d699e
Marco Juliano <marco@juliano.de>
2014-06-03 20:49:02 +0200

    add: lookup of of cached knowledge pojos

--- QueryAgent/src/main/java/org/psikeds/queryagent/presenter/jsf/model/KnowledgeRepresentation.java
+++ QueryAgent/src/main/java/org/psikeds/queryagent/presenter/jsf/model/KnowledgeRepresentation.java
@@ -19,30 +19,53 @@ import java.io.Serializable;
 import org.apache.commons.lang.StringUtils;
 
 import org.psikeds.queryagent.interfaces.presenter.pojos.Choices;
+import org.psikeds.queryagent.interfaces.presenter.pojos.Concept;
+import org.psikeds.queryagent.interfaces.presenter.pojos.Concepts;
 import org.psikeds.queryagent.interfaces.presenter.pojos.Errors;
+import org.psikeds.queryagent.interfaces.presenter.pojos.Feature;
+import org.psikeds.queryagent.interfaces.presenter.pojos.FeatureValue;
 import org.psikeds.queryagent.interfaces.presenter.pojos.Knowledge;
+import org.psikeds.queryagent.interfaces.presenter.pojos.KnowledgeEntities;
+import org.psikeds.queryagent.interfaces.presenter.pojos.KnowledgeEntity;
 import org.psikeds.queryagent.interfaces.presenter.pojos.Metadata;
+import org.psikeds.queryagent.interfaces.presenter.pojos.Purpose;
 import org.psikeds.queryagent.interfaces.presenter.pojos.ResolutionResponse;
+import org.psikeds.queryagent.interfaces.presenter.pojos.Variant;
 import org.psikeds.queryagent.interfaces.presenter.pojos.Warnings;
 
 /**
  * The Frontend Representation/Model of our current Knowledge;
  * usually cached within Session-Scope.
  * 
+ * Additionally the Model holds a Reference to every Object within
+ * the Knowledge allowing the GUI to perform information lookups.
+ * 
  * @author marco@juliano.de
  */
 public class KnowledgeRepresentation implements Serializable {
 
   private static final long serialVersionUID = 1L;
+  private static final KnowledgeCache DEFAULT_CACHE = new KnowledgeCache();
 
+  KnowledgeCache cache;
   ResolutionResponse lastResponse;
 
   public KnowledgeRepresentation() {
-    this(null);
+    this(null, null);
   }
 
-  public KnowledgeRepresentation(final ResolutionResponse lastResponse) {
-    this.lastResponse = lastResponse;
+  public KnowledgeRepresentation(final KnowledgeCache cache, final ResolutionResponse lastResponse) {
+    setKnowledgeCache(cache);
+    setLastResponse(lastResponse);
+  }
+
+  public KnowledgeCache getKnowledgeCache() {
+    return this.cache;
+  }
+
+  public void setKnowledgeCache(final KnowledgeCache cache) {
+    clearCache();
+    this.cache = (cache == null ? DEFAULT_CACHE : cache);
   }
 
   public ResolutionResponse getLastResponse() {
@@ -51,49 +74,176 @@ public class KnowledgeRepresentation implements Serializable {
 
   public void setLastResponse(final ResolutionResponse lastResponse) {
     this.lastResponse = lastResponse;
+    updateCache();
   }
 
   // ----------------------------------------------------------------
 
+  /**
+   * @return Metadata
+   * @see org.psikeds.queryagent.interfaces.presenter.pojos.BaseResolutionContext#getMetadata()
+   */
   public Metadata getMetadata() {
-    return getLastResponse().getMetadata();
+    return this.lastResponse.getMetadata();
   }
 
+  /**
+   * @return Knowledge
+   * @see org.psikeds.queryagent.interfaces.presenter.pojos.BaseResolutionContext#getKnowledge()
+   */
   public Knowledge getKnowledge() {
-    return getLastResponse().getKnowledge();
+    return this.lastResponse.getKnowledge();
   }
 
+  /**
+   * @return Warnings
+   * @see org.psikeds.queryagent.interfaces.presenter.pojos.ResolutionResponse#getWarnings()
+   */
   public Warnings getWarnings() {
-    return getLastResponse().getWarnings();
+    return this.lastResponse.getWarnings();
   }
 
+  /**
+   * @return Errors
+   * @see org.psikeds.queryagent.interfaces.presenter.pojos.ResolutionResponse#getErrors()
+   */
   public Errors getErrors() {
-    return getLastResponse().getErrors();
+    return this.lastResponse.getErrors();
   }
 
+  /**
+   * @return Choices
+   * @see org.psikeds.queryagent.interfaces.presenter.pojos.ResolutionResponse#getChoices()
+   */
   public Choices getChoices() {
-    return getLastResponse().getChoices();
+    return this.lastResponse.getChoices();
   }
 
   // ----------------------------------------------------------------
 
+  /**
+   * @return true if resolved, false else
+   * @see org.psikeds.queryagent.interfaces.presenter.pojos.ResolutionResponse#isResolved()
+   */
   public boolean isResolved() {
     return (this.lastResponse != null) && this.lastResponse.isResolved();
   }
 
+  /**
+   * @return true if there is no response or there are any errors, false else
+   * @see org.psikeds.queryagent.interfaces.presenter.pojos.ResolutionResponse#hasErrors()
+   */
   public boolean hasErrors() {
     return (this.lastResponse == null) || this.lastResponse.hasErrors();
   }
 
+  /**
+   * @return true if there are any warnings, false else
+   * @see org.psikeds.queryagent.interfaces.presenter.pojos.ResolutionResponse#hasWarnings()
+   */
   public boolean hasWarnings() {
     return (this.lastResponse != null) && this.lastResponse.hasWarnings();
   }
 
+  /**
+   * @return SessionID
+   * @see org.psikeds.queryagent.interfaces.presenter.pojos.ResolutionResponse#getSessionID()
+   */
   public String getSessionID() {
     return (this.lastResponse == null ? null : this.lastResponse.getSessionID());
   }
 
+  /**
+   * @return true if there is no SessionID or no Knowledge, false else
+   * @see org.psikeds.queryagent.interfaces.presenter.pojos.ResolutionResponse#isNotInitialized()
+   */
   public boolean isNotInitialized() {
     return (StringUtils.isEmpty(getSessionID()) || (getKnowledge() == null));
+  }
+
+  // ----------------------------------------------------------------
+
+  /**
+   * @param id
+   * @return Concept
+   * @see org.psikeds.queryagent.presenter.jsf.model.KnowledgeCache#getConcept(java.lang.String)
+   */
+  public Concept getConcept(final String id) {
+    return this.cache.getConcept(id);
+  }
+
+  /**
+   * @param id
+   * @return Feature
+   * @see org.psikeds.queryagent.presenter.jsf.model.KnowledgeCache#getFeature(java.lang.String)
+   */
+  public Feature getFeature(final String id) {
+    return this.cache.getFeature(id);
+  }
+
+  /**
+   * @param id
+   * @return FeatureValue
+   * @see org.psikeds.queryagent.presenter.jsf.model.KnowledgeCache#getFeatureValue(java.lang.String)
+   */
+  public FeatureValue getFeatureValue(final String id) {
+    return this.cache.getFeatureValue(id);
+  }
+
+  /**
+   * @param id
+   * @return Variant
+   * @see org.psikeds.queryagent.presenter.jsf.model.KnowledgeCache#getVariant(java.lang.String)
+   */
+  public Variant getVariant(final String id) {
+    return this.cache.getVariant(id);
+  }
+
+  /**
+   * @param id
+   * @return Purpose
+   * @see org.psikeds.queryagent.presenter.jsf.model.KnowledgeCache#getPurpose(java.lang.String)
+   */
+  public Purpose getPurpose(final String id) {
+    return this.cache.getPurpose(id);
+  }
+
+  // ----------------------------------------------------------------
+
+  private void clearCache() {
+    if (this.cache != null) {
+      this.cache.clear();
+    }
+  }
+
+  private void updateCache() {
+    if ((this.cache != null) && (this.lastResponse != null)) {
+      final Knowledge k = this.lastResponse.getKnowledge();
+      final KnowledgeEntities entities = (k == null ? null : k.getEntities());
+      updateCache(entities);
+    }
+  }
+
+  private void updateCache(final KnowledgeEntities entities) {
+    // keep references to all objects in the cache for later lookups by the GUI
+    if ((entities != null) && !entities.isEmpty()) {
+      for (final KnowledgeEntity ke : entities) {
+        if (ke != null) {
+          this.cache.put(ke.getFeatures());
+          this.cache.put(ke.getPurpose());
+          final Variant v = ke.getVariant();
+          this.cache.put(v);
+          this.cache.put(v.getFeatures());
+          final Concepts cons = v.getConcepts();
+          for (final Concept c : cons) {
+            if (c != null) {
+              this.cache.put(c);
+              this.cache.put(c.getValues());
+            }
+          }
+          updateCache(ke.getChildren());
+        }
+      }
+    }
   }
 }
