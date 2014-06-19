7da19ceda52d1e3275bbe6baf8e82cfd7cc26a5e
Marco Juliano <marco@juliano.de>
2014-06-19 19:29:09 +0200

    initial checkin

---
+++ ResolutionEngine/src/main/java/org/psikeds/resolutionengine/util/ContextHelper.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * psiKeds :- ps induced knowledge entity delivery system
+ *
+ * Copyright (c) 2013, 2014 Karsten Reincke, Marco Juliano, Deutsche Telekom AG
+ *
+ * This file is free software: you can redistribute
+ * it and/or modify it under the terms of the
+ * [x] GNU Affero General Public License
+ * [ ] GNU General Public License
+ * [ ] GNU Lesser General Public License
+ * [ ] Creatice Commons ShareAlike License
+ *
+ * For details see file LICENSING in the top project directory
+ *******************************************************************************/
+package org.psikeds.resolutionengine.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Helper for handling Context-Paths
+ * 
+ * @author marco@juliano.de
+ * 
+ */
+public abstract class ContextHelper {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(ContextHelper.class);
+
+  private ContextHelper() {
+    // prevent instantiation
+  }
+
+  // TODO
+}
